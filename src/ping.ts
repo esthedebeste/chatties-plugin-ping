fc9087c0639e1c23e2aee5261d8a204acb5e19d7
esthedebeste <57283066+esthedebeste@users.noreply.github.com>
2023-06-11 21:33:21 +0200

    1.2.0: Added channel ping, talking speed, and voice selection now uses the new string-enum option.

--- src/ping.ts
+++ src/ping.ts
@@ -4,7 +4,8 @@ import type { Plugin, PluginContext } from "chatties/types/plugin-api"
 let context: PluginContext
 let sound: HTMLAudioElement
 
-function matches(message: PrivMessage) {
+function matches(message: PrivMessage): "match" | "channel-ping" | false {
+	if (Date.now() - message.timestamp.getTime() > 1000 * 30) return false // Only ping for messages that are less than 30 seconds old
 	const text = message.message_text.toLowerCase()
 	const login = context.user.login()
 	if (
@@ -12,16 +13,23 @@ function matches(message: PrivMessage) {
 		(context.setting<boolean>("match-username")
 			? new RegExp(`\\b${login}\\b`).test(text)
 			: new RegExp(`\\b@${login}\\b`).test(text))
-	) {
-		return true
-	}
-	const setting = context.setting<string>("regex")
-	if (setting) {
-		const regex = new RegExp(setting)
-		if (regex.test(text)) {
-			return true
-		}
-	}
+	)
+		return "match"
+
+	const alwaysPingChannels = new Set(
+		context
+			.setting<string>("ping-channels")
+			.split(",")
+			.map(s => s.trim())
+	)
+
+	const onlyViewing = context.setting<boolean>("ping-channels-only-viewing")
+	if (
+		(!onlyViewing || message.channel.login === context.visual.currentChannel()) &&
+		(alwaysPingChannels.has("*") || alwaysPingChannels.has(message.channel.login))
+	)
+		return "channel-ping"
+
 	return false
 }
 
@@ -33,10 +41,16 @@ export const plugin = {
 	},
 	message(message) {
 		const text = message.message_text.toLowerCase()
-		if (matches(message)) {
+		const match = matches(message)
+		if (match) {
 			sound.play()
 			if (context.setting<boolean>("tts")) {
-				const format = context.setting<string>("tts-format")
+				const format = context.setting<string>(
+					match === "match"
+						? "tts-format"
+						: /* match === "channel-ping" ? */
+						  "tts-format-channel-ping"
+				)
 				const channel = message.channel.login
 				const sender = message.sender.login
 				const tts = format
@@ -44,8 +58,13 @@ export const plugin = {
 					.replaceAll("{sender}", sender)
 					.replaceAll("{message}", text)
 				const utterance = new SpeechSynthesisUtterance(tts)
-				const voice = speechSynthesis.getVoices()[context.setting<number>("tts-voice")]
-				if (voice) utterance.voice = voice
+				const voiceSetting = context.setting<string>("tts-voice")
+				if (voiceSetting) {
+					const index = Number.parseInt(voiceSetting)
+					const voice = speechSynthesis.getVoices()[index]
+					utterance.voice = voice
+				}
+				utterance.rate = context.setting<number>("tts-speed")
 				speechSynthesis.speak(utterance)
 			}
 		}
@@ -70,20 +89,41 @@ export const plugin = {
 				sound.src = value
 			},
 		},
+		"ping-channels": {
+			name: "Comma-seperated list of channels to trigger a ping for. `*` means any channel",
+			type: "string",
+			default: "",
+		},
+		"ping-channels-only-viewing": {
+			name: "Only do channel-ping if you are currently looking at that channel",
+			type: "boolean",
+			default: true,
+		},
 		tts: {
 			name: "Enable text-to-speech (reads out the location of pings)",
 			type: "boolean",
 			default: false,
+		},
+		"tts-speed": {
+			name: "Rate at which the TTS talks.",
+			type: "number",
+			default: 1,
 		},
 		"tts-format": {
 			name: "TTS format to speak",
 			type: "string",
 			default: "Ping in {channel}. {sender} said: '{message}'",
 		},
+		"tts-format-channel-ping": {
+			name: "TTS format to speak when sourced from a channel ping.",
+			type: "string",
+			default: "Ping in {channel}. {sender} said: '{message}'",
+		},
 		"tts-voice": {
-			name: "TTS Voice Index",
-			type: "number",
-			default: 0,
+			name: "TTS Voice",
+			type: "string-enum",
+			options: [] as string[],
+			default: "",
 		},
 	},
 } satisfies Plugin
@@ -91,10 +131,8 @@ export const plugin = {
 speechSynthesis.addEventListener("voiceschanged", () => {
 	const voices = speechSynthesis.getVoices()
 	console.info("[chatties-plugin-ping] TTS voices changed!", voices)
-	plugin.settings["tts-voice"].name = `TTS Voice Index - ${voices
-		.map(
-			(voice, index) =>
-				`${index} for ${voice.name}${voice.localService ? " (LOCAL)" : " (USES REMOTE SERVICE)"}`
-		)
-		.join(", ")}`
+	plugin.settings["tts-voice"].options = voices.map(
+		(voice, index) =>
+			`${index} - ${voice.name}${voice.localService ? " (LOCAL)" : " (USES REMOTE SERVICE)"}`
+	)
 })
